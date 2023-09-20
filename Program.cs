397e52fce4aacbb283002f11500fe7bb38e89aad
Sachintha Dilshan <sachinthadilshan335@gmail.com>
2023-09-20 18:41:02 +0530

    New updates

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Hostel_Management_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new UserLogin());
+            Application.Run(new Employee());
         }
     }
 }
